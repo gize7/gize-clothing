d02d57c89a3f36b2872d3fc6119255a828d6ef86
gize7 <gize.ajigo@gmail.com>
2021-01-13 13:08:26 +0200

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,8 +1,8 @@
 import firebase from "firebase/app";
-import "firebase/firebase-firestore";
+import "firebase/firestore";
 import "firebase/auth";
 
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyCqUa_4VBy9zmuPVpjYCF1xjdCbS6XqhV0",
   authDomain: "gize-clothing-db.firebaseapp.com",
   projectId: "gize-clothing-db",
@@ -12,13 +12,38 @@ const firebaseConfig = {
   measurementId: "G-TGRFDCESF3",
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
