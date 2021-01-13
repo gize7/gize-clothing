445884431d8b9133823bdfcaf7c3fd411995125c
gize7 <gize.ajigo@gmail.com>
2021-01-13 13:25:40 +0200

    finishing sign in  and sign up  handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
 
@@ -14,15 +14,27 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(
+        "Output => ~ file: sign-in.component.jsx ~ line 25 ~ SignIn ~ handleSubmit= ~ error",
+        error
+      );
+    }
+
     this.setState({ email: "", password: "" });
   };
 
-  handleChange = (event) => {
-    const { value, name } = event.value;
-    this.setState({ [name]: value });
-  };
+  handleChange = event => {
+    const {name, value} = event.target;
+    this.setState({[name]: value})
+}
 
   render() {
     return (
@@ -38,6 +50,7 @@ class SignIn extends Component {
             required
             handleChange={this.handleChange}
             label="email"
+            required
           />
           <FormInput
             name="password"
@@ -46,8 +59,9 @@ class SignIn extends Component {
             required
             handleChange={this.handleChange}
             label="password"
+            required
           />
-          <div className='buttons'>
+          <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
