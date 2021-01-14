dfb47f7fc61a18c6dd7911f8c1330c8228aa7c48
gize7 <gize.ajigo@gmail.com>
2021-01-14 11:46:32 +0200

    added cart icon and cart dropdown components, also implemented cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import cartReducer from "./cart/cart.reducer";
 
 import useReducer from "./user/user.reducer";
 
 export default combineReducers({
   user: useReducer,
+  cart: cartReducer,
 });
