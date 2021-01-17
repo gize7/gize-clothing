cf5d9887a64e8589e4b45596b6d90dca826a592c
gize7 <gize.ajigo@gmail.com>
2021-01-17 15:08:27 +0200

    fix error

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { connect } from "react-redux";
+  
+import React from 'react';
+import { connect } from 'react-redux';
 
-import CustomButton from "../custom-button/custom-button.component";
-import CartItem from "../cart-item/cart-item.component";
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-import "./cart-dropdown.styles.scss";
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.map((cartItem) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
         <CartItem key={cartItem.id} item={cartItem} />
       ))}
     </div>
@@ -18,8 +19,7 @@ const CartDropdown = ({ cartItems }) => (
 );
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+  cartItems
 });
 
-nnect(mapStateToProps)(CartDropdown);
-export default co;
+export default connect(mapStateToProps)(CartDropdown);
