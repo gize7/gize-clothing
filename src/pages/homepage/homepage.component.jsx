b04ea64c9ebed46430bfbe3068160d544b529ffb
gize7 <gize.ajigo@gmail.com>
2021-01-21 17:58:10 +0200

    adding styled-components, converted homepage, header and custom button over to styled-components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
-
-import './homepage.styles.scss';
+import { HomePgeContainer } from "./homepage.styles";
 
 const HomePage = () => (
-    <div className="homepage">
+    <HomePgeContainer>
         <Directory />
-    </div>
+    </HomePgeContainer>
 );
 export default HomePage;
